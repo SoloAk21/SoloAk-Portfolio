3353359a505efe2a4e7e8671d0fc3f546f440de8
Solomon-Asefa <soloasefa6603@gmail.com>
2025-03-20 15:53:07 +0300

    title changed with logo

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import skills from "./skills-experience.svg";
 import skillsBg from "./skills-experience-bg.webp";
 import team from "./team.svg";
@@ -7,7 +6,6 @@ import vision from "./vision.svg";
 import visionBg from "./vision-bg.webp";
 
 export const assets = {
-  logo,
   skills,
   skillsBg,
   team,
